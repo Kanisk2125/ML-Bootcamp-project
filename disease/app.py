b29b3dfd89a698fd79dea209a84a6c264eccde09
Kanisk2125 <105097052+Kanisk2125@users.noreply.github.com>
2023-06-25 22:22:19 +0530

    Update app.py

--- disease/app.py
+++ disease/app.py
@@ -1,7 +1,7 @@
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-import pandas as pd
+# import pandas as pd
 
 # diabetes_model = pd.read_csv(open('diabetes_model.csv','rb'))
 
